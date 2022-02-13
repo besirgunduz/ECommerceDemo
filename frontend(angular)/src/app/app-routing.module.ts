10344dddd1924da9dcb3b48baf3eb2e77490c888
Besir Gunduz <besirgunduz1993@gmail.com>
2022-02-13 20:48:37 +0300

    Update package

--- frontend(angular)/src/app/app-routing.module.ts
+++ frontend(angular)/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ProductComponent } from './components/product/product.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", component: ProductComponent },
+  { path: "products", component: ProductComponent },
+  { path: "products/category/:categoryId", component: ProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
