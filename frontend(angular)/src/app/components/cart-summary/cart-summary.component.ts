a53c84fd25c49c4cf99b4bcc88fd2dd6b831bdf9
Besir Gunduz <besirgunduz1993@gmail.com>
2022-06-18 11:18:21 +0300

    cart summary

---
+++ frontend(angular)/src/app/components/cart-summary/cart-summary.component.ts
@@ -0,0 +1,18 @@
+import { CartItem } from './../../models/cartItem';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-summary',
+  templateUrl: './cart-summary.component.html',
+  styleUrls: ['./cart-summary.component.css']
+})
+export class CartSummaryComponent implements OnInit {
+
+  cartItems:CartItem[];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
