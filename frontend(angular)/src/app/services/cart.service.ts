b4c2e7313992329b99778b7df6f2917780dcbb6f
Besir Gunduz <besirgunduz1993@gmail.com>
2022-06-18 11:17:21 +0300

    cart service

---
+++ frontend(angular)/src/app/services/cart.service.ts
@@ -0,0 +1,29 @@
+import { CartItems } from './../models/cartItems';
+import { Product } from './../models/product';
+import { Injectable } from '@angular/core';
+import { CartItem } from '../models/cartItem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+
+  addToCart(product: Product) {
+    let item = CartItems.find(c => c.product.id === product.id);
+    if (item) {
+      item.quantity += 1;
+    } else {
+      let cartItem = new CartItem();
+      cartItem.product = product;
+      cartItem.quantity = 1;
+      CartItems.push(cartItem)
+    }
+  }
+
+  list():CartItem[]{
+    return CartItems;
+  }
+
+}
