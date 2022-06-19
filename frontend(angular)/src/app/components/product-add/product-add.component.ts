4bd7f33f1d1e7d3f41f566e600309e23a5ecd059
Besir Gunduz <besirgunduz1993@gmail.com>
2022-06-19 11:11:27 +0300

    update add()

--- frontend(angular)/src/app/components/product-add/product-add.component.ts
+++ frontend(angular)/src/app/components/product-add/product-add.component.ts
@@ -28,13 +28,22 @@ export class ProductAddComponent implements OnInit {
   }
 
   add() {
+    debugger;
     if (this.productAddForm.valid) {
+
       let productModel = Object.assign({}, this.productAddForm.value);
       this.productService.add(productModel).subscribe(response => {
         this.toastrService.success(response.message, "Başarılı")
-      },responseError=>{
-        console.log(responseError);
-      });
+      },
+
+        responseError => {
+          if (responseError.error.Errors.length > 0) {
+            for (let i = 0; i < responseError.error.Errors.length; i++) {
+              this.toastrService.error(responseError.error.Errors[i].ErrorMessage, "Doğrulama hatası");
+
+            }
+          }
+        });
 
     } else {
       this.toastrService.error("Formunuz eksik.", "Dikkat")
