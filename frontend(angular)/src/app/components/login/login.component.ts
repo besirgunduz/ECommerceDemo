dd4931740cb885090148e213970197957f69387f
Besir Gunduz <besirgunduz1993@gmail.com>
2022-06-30 23:53:13 +0300

    response model

--- frontend(angular)/src/app/components/login/login.component.ts
+++ frontend(angular)/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +10,32 @@ import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm:FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.createLoginForm();
   }
 
-  createLoginForm(){
+  createLoginForm() {
     this.loginForm = this.formBuilder.group({
-      email:["",Validators.required],
-      password:["",Validators.required]
+      email: ["", Validators.required],
+      password: ["", Validators.required]
     })
   }
 
-  login(){
-    if(this.loginForm.valid){
-      console.log(this.loginForm.value);
+  login() {
+    if (this.loginForm.valid) {
+
+      let loginModel = Object.assign({}, this.loginForm.value)
+
+      this.authService.login(loginModel).subscribe(response => {
+        this.toastrService.info(response.message);
+        localStorage.setItem("token", response.data.token)
+      }, responseError => {
+        this.toastrService.error(responseError.error)
+      }
+      )
     }
   }
 
--- frontend(angular)/src/app/services/auth.service.ts
+++ frontend(angular)/src/app/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { SingleResponseModel } from './../models/singleResponseModel';
+import { TokenModel } from './../models/tokenModel';
 import { HttpClient } from '@angular/common/http';
 import { LoginModel } from './../models/loginModel';
 import { Injectable } from '@angular/core';
@@ -7,10 +9,19 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
 
-  apiUrl = "https://localhost:44379/api/auth";
+  apiUrl = "https://localhost:44379/api/auth/";
   constructor(private httpClient: HttpClient) { }
 
-  login(loginModel:LoginModel){
-    return this.httpClient.post(this.apiUrl+"login",loginModel)
+  login(loginModel: LoginModel) {
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl + "login", loginModel)
+  }
+
+  isAuthenticated() {
+    if (localStorage.getItem("token")) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 }
