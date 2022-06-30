d43f850837b8a2c47afe0e4079d6beee677054b6
Besir Gunduz <besirgunduz1993@gmail.com>
2022-06-30 23:05:25 +0300

    auth işlemleri

---
+++ frontend(angular)/src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { LoginModel } from './../models/loginModel';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = "https://localhost:44379/api/auth";
+  constructor(private httpClient: HttpClient) { }
+
+  login(loginModel:LoginModel){
+    return this.httpClient.post(this.apiUrl+"login",loginModel)
+  }
+}
